1fdfc41c4d3a0965c7ebdfc6c74e1654017e9350
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-07-06 12:25:37 -0400

    fix bullets not moving

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -8,9 +8,9 @@ func _ready():
 func _physics_process(delta):
 	global_position += Vector2(0, -1) * speed * delta
 		
-func _on_visible_on_screen_notified_2d_screen_exited():
-	queue_free()
-
 func _on_area_entered(area):
 	if area is Enemy:
 		queue_free()
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
